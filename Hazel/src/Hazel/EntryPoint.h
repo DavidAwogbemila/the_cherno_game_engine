5013f8c9354e3cd85e68958ba259b8e3f97aa147
DavidAwgbemila <david.awogbemila@bison.howard.edu>
2019-06-13 19:45:09 -0400

    Set up Project with Premake.

---
+++ Hazel/src/Hazel/EntryPoint.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "stdio.h"
+
+#ifdef HZ_PLATFORM_WINDOWS
+
+extern Hazel::Application* Hazel::CreateApplication();
+
+int main(int argc, char** argv) {
+  printf("Entered the Hazel Engine.\n");
+
+  Hazel::Log::Init();
+
+  HZ_CORE_WARN("Core logger issuing a warning!");
+  HZ_INFO("Core logger furnished info.");
+  int a = 5;
+  HZ_INFO("Hello Var={0}", a);
+ 
+  auto app = Hazel::CreateApplication();
+  app->Run();
+  delete app;
+}
+
+#endif
--- src/Hazel.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// For use by Applications.
-
-#include "Hazel/Application.h"
-
-// --------- ENTRY POINT ----------
-#include "Hazel/EntryPoint.h"
-// --------------------------------
